16a928a5fc95cdc4ca2b0e1f9ef958711dbe219d
rmast <rmast@live.nl>
2024-10-11 19:07:06 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -83,9 +83,6 @@ mkdir -p ./output_zip
 cp -rfv ${RPMBUILD_PATH}/RPMS/x86_64/*.rpm ./output_zip/
 sha256sum ${RPMBUILD_PATH}/RPMS/x86_64/*.rpm > ./output_zip/sha256
 
-### Add patches to artifacts
-zip -r patches.zip patches/
-cp -rfv patches.zip ./output_zip/
 echo
 du -sh ./output_zip
 echo
